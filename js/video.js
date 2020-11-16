c4c5c06bae89ae3251ebbde57532f43ba88470b9
esommer21 <60160612+esommer21@users.noreply.github.com>
2020-11-16 18:29:42 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -5,13 +5,11 @@ window.addEventListener("load", function() {
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
-	vid.play()
+	video.play()
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause Video");
-	vid.pause()
+	video.pause()
 	
 });
 
