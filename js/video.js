a7d2c1c74f98af08147a330a6e6cea4153ae0662
esommer21 <60160612+esommer21@users.noreply.github.com>
2020-11-18 22:17:28 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -38,6 +38,6 @@ document.querySelector("#mute").addEventListener("click", function() {
 		
 });
 document.querySelector("#old").addEventListener("click", function() {
-	video.getElementById("old").classList.toggle("oldTime")
+	document.getElementById("myVideo").classList.toggle("oldTime")
 	
 });	
